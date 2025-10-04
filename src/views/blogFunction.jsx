0af1be58125ddd1026808bb50c20ee54cddabe47
Radhish Arjunan <fussionlab@users.noreply.github.com>
2025-10-04 16:52:15 +0300

    Update blogFunction.jsx

--- src/views/blogFunction.jsx
+++ src/views/blogFunction.jsx
@@ -1,12 +1,8 @@
-import axios from 'axios';
-
- const fetchViewsLikes = () =>{
-    axios
-    .get("http://localhost:81/phpapi/api/views/readonce/?id" + this.props.match.params.id)
+const fetchViewsLikes = () =>{
+    fetch("http://localhost:81/phpapi/api/views/readonce/?id" + this.props.match.params.id)
     .then(res => {
       const viewLikeList = res.data.item;
       this.setState({ viewLikeList });
-      console.log(...this.state.viewLikeList);
     })
 
     .catch(err => console.log(err));
